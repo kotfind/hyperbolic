6a4b5df2540093f30ae8d692e8e7899907214c8b
kotfind <kotfind@yandex.ru>
2022-07-17 12:48:53 +0300

    Fix hyperbolic

--- src/HyperbolicEngine.cpp
+++ src/HyperbolicEngine.cpp
@@ -2,18 +2,10 @@
 
 #include <QDebug>
 #include <math.h>
-#include <QRandomGenerator>
 
 const double speed = 0.005;
 
 HyperbolicEngine::HyperbolicEngine() {
-    QRandomGenerator rnd;
-    for (int i = 0; i < 300; ++i) {
-        auto r = rnd.generateDouble();
-        auto a = rnd.generateDouble() * 2 * M_PI;
-
-        tail.push_back(r * Vector(cos(a), sin(a), 0));
-    }
 }
 
 void HyperbolicEngine::move(double horDir, double vertDir) {
@@ -24,9 +16,9 @@ void HyperbolicEngine::move(double horDir, double vertDir) {
 }
 
 Vector applyOffset(const Vector& pt, const Vector& offset) {
-    const auto& x = offset;
-    auto v = pt; // XXX
-    v.z = 0;
+    auto x = pt; // XXX
+    x.z = 0;
+    const auto& v = offset;
     return (v * (1 + 2 * v * x + x * x) + x * (1 - v * v)) /
         // --------------------------------------------------
             (1 + 2 * v * x + v * v * x * x);
