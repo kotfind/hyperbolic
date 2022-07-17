0188a087c08046f7de3bdf20e832cdc750928aa3
kotfind <kotfind@yandex.ru>
2022-07-17 14:38:18 +0300

    Improve leapmotion controll

--- src/LeapMotion.cpp
+++ src/LeapMotion.cpp
@@ -1,6 +1,7 @@
 #include "LeapMotion.h"
 
 #include <QDebug>
+#include <math.h>
 
 LeapMotion::LeapMotion()
         : listener(horDir, vertDir, validDirs) {
@@ -13,12 +14,18 @@ LeapMotion::Listener::Listener(double& horDir, double& vertDir, bool& validDirs)
         : horDir(horDir), vertDir(vertDir), validDirs(validDirs) {
 }
 
+double f(double x) {
+    int sgn = (x > 0) - (x < 0);
+    x = abs(x);
+    return sgn * std::max(0., x - 20) / 50;
+}
+
 void LeapMotion::Listener::onFrame(const Leap::Controller& controller) {
     const auto& hands = controller.frame().hands();
     if (!(validDirs = !hands.isEmpty())) return;
 
-    const auto& hand = hands[0];
+    const auto& pos = hands[0].palmPosition();
 
-    horDir = -hand.palmNormal().roll() / M_PI * 2;
-    vertDir = -hand.direction().pitch() / M_PI * 5;
+    horDir = f(pos.x);
+    vertDir = -f(pos.z);
 }
